33557a66ea80aa1ec76aa6ecdff9ca23df15cf57
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-12-14 19:41:33 +0700

    #fixed: error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
