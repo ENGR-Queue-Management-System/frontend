afd8125b12a7d10371c7bbda9b0c5c4d8b7336c5
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-12-22 03:47:08 +0700

    #fixed: _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,6 +28,10 @@ export default function Document() {
           name="apple-mobile-web-app-status-bar-style"
           content="black-translucent"
         />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        ></meta>
       </Head>
       <body>
         <Main />
