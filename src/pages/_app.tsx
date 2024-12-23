0dbdd0b9e04d11ea9f5bc425544d239e59438502
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2024-12-23 14:43:34 +0700

    #fixed: button subscribe

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,6 +23,7 @@ import { setLoading } from "@/store/loading";
 import LoadingOverlay from "@/components/LoadingOverlay";
 import { toast } from "@/hooks/use-toast";
 import { ToastAction } from "@/components/ui/toast";
+import { Button } from "@/components/ui/button";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { isSupported, isGranted, handleSubscribe } = useNotification();
@@ -39,21 +40,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     }, 2000);
     return () => clearTimeout(timeout);
   }, []);
-
-  useEffect(() => {
-    if (isSupported && !isGranted) {
-      toast({
-        title: "Subscribe Notifications!",
-        variant: "default",
-        className: "mt-16",
-        action: (
-          <ToastAction altText="subscribe" onClick={handleSubscribe}>
-            Subscribe
-          </ToastAction>
-        ),
-      });
-    }
-  }, [isSupported]);
 
   useEffect(() => {
     if (!counters.length) {
@@ -103,6 +89,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     </div>
   ) : !isSupported ? (
     <UnsupportedNotification />
+  ) : !isGranted ? (
+    <div className="flex flex-col h-screen w-screen justify-center items-center overflow-hidden">
+      <Button onClick={handleSubscribe}>Subscribe</Button>
+    </div>
   ) : loading.loadingOverlay ? (
     <LoadingOverlay />
   ) : (
