f70ecfd7cfe9ac825cf3e0525e0cc045250bb9da
auswitch <90751246+auswitch@users.noreply.github.com>
2024-12-23 18:30:46 +0700

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,11 @@ import type { AppProps } from "next/app";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { Provider } from "react-redux";
+import Image from "next/image";
+import logoEngColor from "../../public/images/logoSDColor.png";
+import logoEng from "../../public/images/logoSDShadow.png";
+import iconEx from "../../public/icons/exclaimation.svg";
+import iconBell from "../../public/icons/bell.svg";
 import {
   NotificationProvider,
   useNotification,
@@ -21,8 +26,11 @@ import { Toaster } from "@/components/ui/toaster";
 import { setLoading } from "@/store/loading";
 import LoadingOverlay from "@/components/LoadingOverlay";
 import { Button } from "@/components/ui/button";
+import { DEVICE_TYPE } from "@/config/Enum";
+import Icon from "@/components/Icon";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { deviceType } = useNotification();
   const { isSupported, isGranted, handleSubscribe } = useNotification();
   const loading = useAppSelector((state) => state.loading);
   const router = useRouter();
@@ -87,8 +95,114 @@ function MyApp({ Component, pageProps }: AppProps) {
   ) : !isSupported ? (
     <UnsupportedNotification />
   ) : !isGranted ? (
-    <div className="flex flex-col h-screen w-screen justify-center items-center overflow-hidden">
-      <Button onClick={handleSubscribe}>Subscribe</Button>
+    <div
+      className={` ${
+        [DEVICE_TYPE.IOS, DEVICE_TYPE.ANDROID].includes(deviceType!) 
+          ? "flex-col flex !h-full w-full"
+          : "flex h-screen  w-screen justify-center text-center items-center overflow-hidden"
+      }  `}
+    >
+      <div
+        className={`${
+          [DEVICE_TYPE.IOS, DEVICE_TYPE.ANDROID].includes(deviceType!) 
+            ? " iphone:max-sm:hidden"
+            : "gradient-bg text-[1.8vw] font-semibold text-white flex flex-col h-screen w-[55%] text-start justify-center  pl-[4vw]"
+        }`}
+      >
+        <Image
+          src={
+            [DEVICE_TYPE.IOS, DEVICE_TYPE.ANDROID].includes(deviceType!) 
+              ? logoEngColor
+              : logoEng
+          }
+          alt="logoEng"
+          className={`samsungA24:w-[10vw] ${
+            [DEVICE_TYPE.IOS, DEVICE_TYPE.ANDROID].includes(deviceType!) 
+              ? " mt-[50px] mb-4 "
+              : "-ml-3 mt-5 mb-3"
+          } acerSwift:max-macair133:w-[12vw] cursor-not-allowed macair133:max-samsungA24:w-[11vw] iphone:max-sm:hidden sm:max-macair133:w-[20vw]`}
+        />
+        <p>Welcome to </p>
+        <p>Automatic Queuing System</p>
+        <div>
+          <p className=" mt-4  sm:max-samsungA24:text-[14px] acerSwift:max-macair133:text-[16px] font-[500] iphone:max-sm:text-[12px] text-[16px]">
+            Student Development Room <br />
+            Faculty of Engineering, Chiang Mai University
+          </p>
+        </div>
+      </div>
+
+      <div
+        className={` ${
+          [DEVICE_TYPE.IOS, DEVICE_TYPE.ANDROID].includes(deviceType!) 
+            ? " !w-full !h-screen  !justify-end !items-start pb-16 !text-start bg-gradient-to-b from-[#22a0a0]/20 to-white"
+            : "w-[45%]"
+        } flex flex-col  justify-center text-center items-center`}
+      >
+        <div
+          className={`flex flex-col ${
+            [DEVICE_TYPE.IOS, DEVICE_TYPE.ANDROID].includes(deviceType!) 
+              ? "w-[100%] px-6 justify-start items-start text-start"
+              : "w-[80%] justify-center items-center text-center"
+          } `}
+        >
+          {" "}
+          <Icon
+            IconComponent={iconBell}
+            className={`text-[#22a0a0] size-20 stroke-[1.4px] mb-3 ${
+              [DEVICE_TYPE.IOS, DEVICE_TYPE.ANDROID].includes(deviceType!) 
+                ? "-ml-2"
+                : ""
+            }`}
+          />
+          <p
+            className={`${
+              [DEVICE_TYPE.IOS, DEVICE_TYPE.ANDROID].includes(deviceType!) 
+                ? "text-[#22a0a0] text-start font-semibold text-[3vh]  "
+                : "text-[#22a0a0] font-semibold text-[1.2vw]"
+            } `}
+          >
+            {" "}
+            Get notified!
+          </p>{" "}
+          <p
+            className={`${
+              [DEVICE_TYPE.IOS, DEVICE_TYPE.ANDROID].includes(deviceType!) 
+                ? "text-default text-start font-semibold text-[2.5vh] "
+                : "hidden"
+            } `}
+          >
+            {" "}
+            Automatic Queuing System
+          </p>
+          <p className="my-4 text-[15px] text-default font-medium">
+            Don’t miss your queue! Allow notifications, and we’ll send you a
+            friendly alert as soon as your queue arrives.
+          </p>
+          <div
+            className={`flex gap-3  items-center justify-center w-[65%] ${
+              [DEVICE_TYPE.IOS, DEVICE_TYPE.ANDROID].includes(deviceType!)
+                ? " w-[90%] mt-2"
+                : "w-[100%] mt-8"
+            } acerSwift:max-macair133:w-[40vw] p-4 acerSwift:max-macair133:p-3 rounded-md bg-[#f73b3b]/15`}
+          >
+            <Icon IconComponent={iconEx} className="text-[#f73b3b]" />
+            <p className="iphone:max-sm:text-[14px] text-[14px] acerSwift:max-macair133:text-b3 text-[#f73b3b] font-bold text-start w-full ">
+              Notifications Required <br />
+              <p className="font-medium mt-[2px]">
+                If you do not allow notifications, you won’t be able to access
+                the system.
+              </p>
+            </p>
+          </div>
+          <Button className={`mt-5 ${
+              [DEVICE_TYPE.IOS, DEVICE_TYPE.ANDROID].includes(deviceType!) 
+                ? " w-[100%] mt-5 h-12 text-[15px] font-semibold"
+                : ""}`} onClick={handleSubscribe}>
+            Allow Notification
+          </Button>
+        </div>{" "}
+      </div>
     </div>
   ) : loading.loadingOverlay ? (
     <LoadingOverlay />
